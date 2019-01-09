bc7bcd41fec9013769958a4d8aa7fecca8efbfdb
B <brandon.johnson@generalassemb.ly>
2019-01-09 14:27:39 -0800

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,11 @@
+console.log("Up and running.");
+
+let cardOne, cardTwo, cardThree, cardFour;
+
+cardOne = "queen";
+cardTwo = "king";
+cardThree = "queen";
+cardFour = "king";
+
+console.log(`User flipped ${cardFour}`);
+console.log(`User flipped ${cardTwo}`);
