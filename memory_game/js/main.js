ae6a356eb001b7116d5a41767a9c088a0b6e6c1e
B <brandon.johnson@generalassemb.ly>
2019-01-11 08:56:08 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,11 +1,21 @@
 console.log("Up and running.");
 
-let cardOne, cardTwo, cardThree, cardFour;
+let cards = ["queen", "queen", "king", "king"];
+let cardsInPlay = [];
 
-cardOne = "queen";
-cardTwo = "king";
-cardThree = "queen";
-cardFour = "king";
+let cardOne = cards[0];
+let cardTwo = cards[2];
 
-console.log(`User flipped ${cardFour}`);
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log(`User flipped ${cardOne}`);
 console.log(`User flipped ${cardTwo}`);
+
+if (cardsInPlay.length === 2) {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert(`You found a match!`);
+  } else {
+    alert(`Sorry, try again.`);
+  }
+}
